e99ce925927aa728761b817256b6803e2e149905
Klavier <klaviernavarro@gmail.com>
2022-09-20 18:07:43 +0200

    Shoots added

--- 01 - Marcianos/Assets/Scripts/Nave.cs
+++ 01 - Marcianos/Assets/Scripts/Nave.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class Nave : MonoBehaviour
 {
     [SerializeField] float velocidad = 2;
+    [SerializeField] Transform prefabDisparo;
+    private float velocidadDisparo = 4;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +21,18 @@ public class Nave : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         transform.Translate(horizontal * velocidad * Time.deltaTime, 0, 0);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Instantiate(prefabDisparo, transform.position, Quaternion.identity);
+            Transform disparo = Instantiate(prefabDisparo, transform.position, 
+                Quaternion.identity);
+            disparo.gameObject.GetComponent<Rigidbody2D>().velocity =
+                new Vector3(0, velocidadDisparo, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        UnityEngine.Debug.Log("Golpeado");
     }
 }
